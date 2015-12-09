73b1fbb0acc7a7d39f9f25289f70ccc8e13ca2a4
sean <seanowenhayes@gmail.com>
2015-12-09 22:39:10 +0100

    Added mrspider css data extractor functionality.

--- index.js
+++ index.js
@@ -3,14 +3,19 @@
 var mrspiderRequest = require('mrspider-request');
 var mrspiderCheerio = require('mrspider-cheerio');
 var mrspiderCssLinks = require('mrspider-css-links');
-var spider = require('mrspider')();
-const LINKS_RULE = '.pagination-next a';
+var mrspiderCssData = require('mrspider-css-data-extractor');
+var spider = require('mrspider')({
+    baseUrl: 'http://www.fotocasa.es'
+});
+const LINKS_RULE = '.pagination-next a, .property-location';
 
 spider.use(mrspiderRequest);
 spider.use(mrspiderCheerio);
 spider.use(mrspiderCssLinks(LINKS_RULE));
+spider.use(mrspiderCssData({title: 'h1.property-title'}));
 spider.use(function(page, spider, next) {
     console.log(page.url);
+    console.log(page.data);
     next();
 });
 spider.addUrl('http://www.fotocasa.es/alquiler/garajes/barcelona-provincia/listado-por-foto/');
