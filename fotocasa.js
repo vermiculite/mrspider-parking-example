1c17dcd061666443037e39b1c14e9db850ef5a33
sean <seanowenhayes@gmail.com>
2016-01-04 14:17:20 +0100

    Updated to use validator and mongo persister.

--- fotocasa.js
+++ fotocasa.js
@@ -6,6 +6,8 @@ var mrspiderCssLinks = require('mrspider-css-links');
 var mrspiderCssData = require('mrspider-css-data-extractor');
 var mrspiderCssImage = require('mrspider-css-image-extraction');
 var regexDataExtractor = require('mrspider-regex-data-extractor');
+var mrspiderValidator = require('mrspider-validator');
+var mongodbPersister = require('mrspider-mongodb-persister');
 var spider = require('mrspider')({
     baseUrl: 'http://www.fotocasa.es'
 });
@@ -33,11 +35,40 @@ spider.use(regexDataExtractor({
     lng: /Lng": "(\d+\.\d+)/
 }));
 
+spider.use(mrspiderValidator({
+    title: {
+        type: 'string',
+        required: true
+    },
+    price: {
+        type: 'string',
+        required: true
+    },
+    detail: {
+        type: 'string'
+    },
+    images: {
+        type: 'array'
+    },
+    lat: {
+        type: 'number'
+    },
+    lng: {
+        type: 'number'
+    }
+}));
+
+spider.use(mongodbPersister({
+    url: 'mongodb://localhost:27017/parking',
+    collection: 'results'
+}));
+
 spider.use(function(page, spider, next) {
     console.log(page.url);
     console.log(page.data);
     next();
 });
-spider.addUrl('http://www.fotocasa.es/alquiler/garajes/barcelona-provincia/listado-por-foto/');
-spider.crawl();
 
+spider.addUrl('http://www.fotocasa.es/alquiler/garajes/barcelona-provincia/listado-por-foto/');
+
+spider.crawl();
