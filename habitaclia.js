746b86d8d21fc2426d537d50588b0617b518efe8
sean <seanowenhayes@gmail.com>
2016-01-06 12:04:57 +0100

    Changed habitaclia to use jsdom but still has encoding issues.

--- habitaclia.js
+++ habitaclia.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var mrspiderRequest = require('mrspider-request');
-var mrspiderCheerio = require('mrspider-cheerio');
+var mrspiderJsdom = require('mrspider-jsdom')();
 var mrspiderCssLinks = require('mrspider-css-links');
 var mrspiderCssData = require('mrspider-css-data-extractor');
 var mrspiderCssImage = require('mrspider-css-image-extraction');
@@ -9,12 +9,12 @@ var regexDataExtractor = require('mrspider-regex-data-extractor');
 var persistence = require('./persistence');
 
 var spider = require('mrspider')({
-    baseUrl: 'http://www.idealista.com'
+    baseUrl: 'http://www.habitaclia.com'
 });
 
 spider.use(mrspiderRequest);
 
-spider.use(mrspiderCheerio);
+spider.use(mrspiderJsdom);
 
 spider.use(mrspiderCssLinks('.paginacionlista a:last-child, h3 a'));
 
